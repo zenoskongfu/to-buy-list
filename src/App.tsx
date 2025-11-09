c3694b6a2878e2dee7ec7c230a01e248edff2870
zenos <zenos.chen@foxmail.com>
2025-11-09 21:31:05 +0800

    添加多个页面

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
 import "./App.css";
+import Login from "./pages/Login";
+import BuyList from "./pages/BuyList";
+import Menu from "./pages/Menu";
+import Loading from "./pages/Loading";
 
 function App() {
-	return <div>zenos</div>;
+	const pathname = window.location.pathname;
+	console.log(pathname);
+
+	if (pathname === "/login") {
+		return <Login />;
+	}
+
+	if (pathname === "/buylist") {
+		return <BuyList />;
+	}
+
+	if (pathname === "/menu") {
+		return <Menu />;
+	}
+
+	return (
+		<div>
+			<Loading />
+		</div>
+	);
 }
 
 export default App;
